7bf039a972d794e499f7bad54332d3977a2703d2
unnnaaaa <114554567+unnnaaaa@users.noreply.github.com>
2025-03-27 17:23:05 +0700

    Add files via upload

---
+++ Tugas6_JS_Hasna Nurul Azmi Pranindya/data.js
@@ -0,0 +1,14 @@
+//data.js
+const users = [
+{nama: 'Data 1', umur: 20, alamat: 'Jl. Data 1'},
+{nama: 'Data 2', umur: 21, alamat: 'Jl. Data 2'},
+{nama: 'Data 3', umur: 22, alamat: 'Jl. Data 3'},
+{nama: 'Data 4', umur: 23, alamat: 'Jl. Data 4', email: 'data4@email.com' },
+{nama: 'Data 5', umur: 24, alamat: 'Jl. Data 5', email: 'data5@email.com' },
+{nama: 'Data 6', umur: 25, alamat: 'Jl. Data 6', email: 'data6@email.com' },
+{nama: 'Data 7', umur: 26, alamat: 'Jl. Data 7', email: 'data7@email.com' },
+{nama: 'Data 8', umur: 27, alamat: 'Jl. Data 8', email: 'data8@email.com' },
+{nama: 'Data 9', umur: 28, alamat: 'Jl. Data 9', email: 'data9@email.com' },
+{nama: 'Data 10', umur: 29, alamat: 'Jl. Data 10', email: 'data10@email.com' }
+];
+export default users;
